1333c6bc5c4fb048d2f8252f7d550f82c613cfdd
shareef <sh.mwafy1@gmail.com>
2024-04-18 19:00:35 +0300

    Edit Testimonial Responsive

---
+++ main.js
@@ -0,0 +1,11 @@
+// const button = document.getQuerySelector("header button");
+
+// button.onClick = function (e){
+//     e.preventDefault();
+//     console.log("hi");
+//     document.querySelector("header").style.width = "500px"
+
+
+// }
+
+
